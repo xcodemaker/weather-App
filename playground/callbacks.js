f82c7b105046ee79bfa00132609e2e00880eb80d
xcodemaker <dhammika.dsk@gmail.com>
2018-03-28 02:41:25 +0530

    start new project weather app

---
+++ playground/callbacks.js
@@ -0,0 +1,16 @@
+var getUser=(id, callback)=>{
+
+    var user={
+        id:id,
+        name :"dhammika"
+    };
+    setTimeout(()=>{
+        callback(user);
+    },3000);
+    
+
+};
+
+getUser(31,(userObject)=>{
+    console.log(userObject);
+});
